573676d332b91b37056ff4c136d54a7b8e33b8f7
Andrew Tridgell <andrew@tridgell.net>
2021-11-07 19:30:47 +1100

    AP_Filesystem: support formatting of SDcard on ChibiOS

--- libraries/AP_Filesystem/AP_Filesystem.cpp
+++ libraries/AP_Filesystem/AP_Filesystem.cpp
@@ -65,6 +65,8 @@ const AP_Filesystem::Backend AP_Filesystem::backends[] = {
 #endif
 };
 
+extern const AP_HAL::HAL& hal;
+
 #define MAX_FD_PER_BACKEND 256U
 #define NUM_BACKENDS ARRAY_SIZE(backends)
 #define LOCAL_BACKEND backends[0]
@@ -246,6 +248,19 @@ FileData *AP_Filesystem::load_file(const char *filename)
     return backend.fs.load_file(filename);
 }
 
+
+// format filesystem
+bool AP_Filesystem::format(void)
+{
+#if AP_FILESYSTEM_FORMAT_ENABLED
+    if (hal.util->get_soft_armed()) {
+        return false;
+    }
+    return LOCAL_BACKEND.fs.format();
+#else
+    return false;
+#endif
+}
 
 namespace AP
 {
