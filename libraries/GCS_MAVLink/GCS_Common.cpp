e329d79342cd1e40f241c8216719b7ab4484d2c9
Andrew Tridgell <andrew@tridgell.net>
2024-02-25 10:01:44 +1100

    GCS_MAVLink: pass channel number to AP_GPS on RTCM injection

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -3894,7 +3894,7 @@ void GCS_MAVLINK::handle_common_message(const mavlink_message_t &msg)
     case MAVLINK_MSG_ID_GPS_INPUT:
     case MAVLINK_MSG_ID_HIL_GPS:
     case MAVLINK_MSG_ID_GPS_INJECT_DATA:
-        AP::gps().handle_msg(msg);
+        AP::gps().handle_msg(chan, msg);
         break;
 
     case MAVLINK_MSG_ID_STATUSTEXT:
